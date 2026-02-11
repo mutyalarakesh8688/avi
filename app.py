aa6f5aa8a26aca1b03ea7b301696ce47b1497dfa
mutyalarakesh8688 <mutyalarakesh8688@gmail.com>
2026-02-11 10:42:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -280,7 +280,8 @@ def logout():
     return redirect(url_for('login'))
 
 # ---------------- RUN APP ---------------- #
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
